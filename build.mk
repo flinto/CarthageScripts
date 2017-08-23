1795a082ce6fccaf6b989e9822450d1f0f914a6d
Kazuho Okui <kazuho@gmail.com>
2017-08-23 11:19:34 -0700

    Update localization script

--- build.mk
+++ build.mk
@@ -22,7 +22,7 @@ localize:
 		find "$$base" -name "*.swift" ! -name "Localize.swift" | xargs genstrings -q -u -s $(ROUTINE); \
 		iconv -f UTF-16LE -t utf8 Localizable.strings > Localizable-utf8.strings; \
 		for lang in $(LANG); do \
-			./CarthageScripts/genstringmerge.rb "$$base"/"$$lang"/Localizable.strings Localizable-utf8.strings; \
+			./CarthageScripts/genstringmerge.rb "$$base"/"Resources/$$lang"/Localizable.strings Localizable-utf8.strings; \
 		done ; \
   done
 
