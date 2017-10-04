d61c67fce8618044317869a646e376462e0710a1
Kazuho Okui <kazuho@gmail.com>
2017-10-04 15:40:58 -0700

    Fix typo in script

--- update_xliff.rb
+++ update_xliff.rb
@@ -17,10 +17,10 @@ doc.xpath("//xliff:trans-unit", "xliff" => "urn:oasis:names:tc:xliff:document:1.
   if source =~ /(__[A-Za-z_]+__)/
     trans.remove
   end
-  if source =~ /(__[A-Za-z_]+__)/
+  if source =~ /(__DEBUG)/
     trans.remove
   end
-  if source =~ /Note=\"Do not translate\";/
+  if trans.at("note").content =~ /Note\s*=\s*\"Do not translate\";/
     trans.remove
   end
   if trans.attr("id") =~ /ibShadowedMultipleValuesPlaceholder|ibShadowedIsNilPlaceholder|ibShadowedNoSelectionPlaceholder|ibShadowedNotApplicablePlaceholder/
