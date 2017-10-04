6200ba8686e82842b3bf108ce452c17ef5ec4c0d
Kazuho Okui <kazuho@gmail.com>
2017-10-04 15:37:59 -0700

    Refine XLIFF generator

--- update_xliff.rb
+++ update_xliff.rb
@@ -11,18 +11,21 @@ doc.xpath("//xliff:file", "xliff" => "urn:oasis:names:tc:xliff:document:1.2").ea
   end
 end
 
-
 doc.xpath("//xliff:trans-unit", "xliff" => "urn:oasis:names:tc:xliff:document:1.2").each do |trans|
   next unless trans.at("note")
   source = trans.at("source").content
   if source =~ /(__[A-Za-z_]+__)/
-    trans.at("source").content = trans.at("source").content.sub '; Note="Do not translate";', ''
-    trans.at("source").content += '; Note="Do not translate";'
+    trans.remove
+  end
+  if source =~ /(__[A-Za-z_]+__)/
+    trans.remove
+  end
+  if source =~ /Note=\"Do not translate\";/
+    trans.remove
   end
   if trans.attr("id") =~ /ibShadowedMultipleValuesPlaceholder|ibShadowedIsNilPlaceholder|ibShadowedNoSelectionPlaceholder|ibShadowedNotApplicablePlaceholder/
     if trans.at("source").content =~ /^(\s+|0)$/
-    trans.at("source").content.sub '; Note="Do not translate";', ''
-    trans.at("source").content += '; Note="Do not translate";'
+    trans.remove
     end
   end
 end
