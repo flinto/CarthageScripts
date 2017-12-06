8ac61c5450322fcaba7cb0a8afdc4a1550cbc8bc
Kazuho Okui <kazuho@gmail.com>
2017-12-06 09:21:02 -0800

    Fix typo

--- archive-framework.sh
+++ archive-framework.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sh ./CarthageScripts/bump-version.sh
 carthage build --verbose --no-skip-current
-xcrun strip -S -T Carthage/Build/Mac/ $FRAMEWORK_NAME.framework/$FRAMEWORK_NAME
-xcrun strip -S -T Carthage/Build/iOS/ $FRAMEWORK_NAME.framework/$FRAMEWORK_NAME
+xcrun strip -S -T Carthage/Build/Mac/$FRAMEWORK_NAME.framework/$FRAMEWORK_NAME
+xcrun strip -S -T Carthage/Build/iOS/$FRAMEWORK_NAME.framework/$FRAMEWORK_NAME
 carthage archive $FRAMEWORK_NAME
