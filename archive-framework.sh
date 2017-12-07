4bd68221b76b830cde7e0fea10e667512af057d2
Kazuho Okui <kazuho@gmail.com>
2017-12-07 09:52:31 -0800

    Add file existing check before stripping symbol

--- archive-framework.sh
+++ archive-framework.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 sh ./CarthageScripts/bump-version.sh
 carthage build --verbose --no-skip-current
-xcrun strip -S -T Carthage/Build/Mac/$FRAMEWORK_NAME.framework/$FRAMEWORK_NAME
-xcrun strip -S -T Carthage/Build/iOS/$FRAMEWORK_NAME.framework/$FRAMEWORK_NAME
+if [ -f Carthage/Build/Mac/$FRAMEWORK_NAME.framework/$FRAMEWORK_NAME ]; then
+  xcrun strip -S -T Carthage/Build/Mac/$FRAMEWORK_NAME.framework/$FRAMEWORK_NAME
+fi
+if [ -f Carthage/Build/iOS/$FRAMEWORK_NAME.framework/$FRAMEWORK_NAME ]; then
+  xcrun strip -S -T Carthage/Build/iOS/$FRAMEWORK_NAME.framework/$FRAMEWORK_NAME
+fi
 carthage archive $FRAMEWORK_NAME
