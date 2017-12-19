fda06e37ecf7106e35bca084e6204f0763fecf8c
Kazuho Okui <kazuho@gmail.com>
2017-12-19 11:57:45 -0800

    Disable stripping framework

--- archive-framework.sh
+++ archive-framework.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
 sh ./CarthageScripts/bump-version.sh
 carthage build --verbose --no-skip-current
-if [ -f Carthage/Build/Mac/$FRAMEWORK_NAME.framework/$FRAMEWORK_NAME ]; then
-  xcrun strip -S -T Carthage/Build/Mac/$FRAMEWORK_NAME.framework/$FRAMEWORK_NAME
-fi
-if [ -f Carthage/Build/iOS/$FRAMEWORK_NAME.framework/$FRAMEWORK_NAME ]; then
-  xcrun strip -S -T Carthage/Build/iOS/$FRAMEWORK_NAME.framework/$FRAMEWORK_NAME
-fi
 carthage archive $FRAMEWORK_NAME
