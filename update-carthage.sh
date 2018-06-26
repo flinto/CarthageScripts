21f49945ff33150702cd1c8b02e424f455fb764f
Kazuho Okui <kazuho@gmail.com>
2018-06-26 12:16:03 -0700

    Add script to checkout pre-built framework via Carthage

---
+++ update-carthage.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+if [ -n "$TRAVIS" ]; then
+  echo "Travis build: Skip checkout. Checkout is done by .travis.yml."
+  exit
+fi
+
+check_symlink() {
+  ls Carthage/Checkouts | while read line
+  do
+      read -a array <<< "$line"
+      dependency=${array[0]}
+
+      if [ -L "Carthage/Checkouts/$dependency" ]
+      then
+        echo "$dependency"
+      fi
+  done
+}
+
+if [ -d ./Carthage/Checkouts ]; then
+  checkouts=`check_symlink`
+fi
+
+if [ ! -z "$checkouts" -a "$checkouts" != " " ]; then
+  echo "Development sym link \"$checkouts\" found"
+fi
+
+frameworks=`ruby ./scripts/get_frameworks.rb $checkouts`
+
+if [ "$?" == "1" ]; then
+  exit
+fi
+if [ "$frameworks" == "" ]; then
+  exit
+fi
+echo "download dependencies... $frameworks"
+
+VERSION=`carthage version`
+if [ "$VERSION" == "0.29.0" ]; then
+  carthage build --use-binaries $frameworks
+else
+  carthage checkout --use-binaries $frameworks
+fi
