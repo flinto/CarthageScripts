9e154127490cb0c458c842d53309aac95ea11753
Kazuho Okui <kazuho@gmail.com>
2018-08-16 11:12:06 -0700

    Update static build script

--- build-static-framework.sh
+++ build-static-framework.sh
@@ -3,9 +3,9 @@
 xcconfig=$(mktemp /tmp/static.xcconfig.XXXXXX)
 trap 'rm -f "$xcconfig"' INT TERM HUP EXIT
 
-echo "LD = $PWD/CarthageScripts/ld.py" >> $xcconfig
+#echo "LD = $PWD/CarthageScripts/ld.py" >> $xcconfig
 echo "DEBUG_INFORMATION_FORMAT = dwarf" >> $xcconfig
 
 export XCODE_XCCONFIG_FILE="$xcconfig"
 
-carthage build "$@" --no-skip-current
+carthage build "$@" --no-skip-current --no-use-binaries
